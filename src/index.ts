b5cf101fdb2b0c5e9b1a1096423e63254284a322
Kim Daniel Engebretsen <kim@fireclover.cloud>
2025-03-15 09:35:43 +0100

    needs testing with a real project deployment

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { DynamicSite } from './dynamic-site';
+
+
+/**
+ * This stack relies on getting the domain name from CDK context.
+ * Use 'cdk synth -c domain=mydynamicsite.com -c subdomain=www'
+ * Or add the following to cdk.json:
+ * {
+ *   "context": {
+ *     "domain": "mydynamicsite.com",
+ *     "subdomain": "www",
+ *     "accountId": "1234567890",
+ *     "webPath": "../web/dist",
+ *     "apiApth": "../api/dist",
+ *   }
+ * }
+**/
+class DynamicSiteStack extends cdk.Stack {
+    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+        super(parent, name, props);
+
+        new DynamicSite(this, 'StaticSite', {
+            domainName: this.node.tryGetContext('domain'),
+            siteSubDomain: this.node.tryGetContext('subdomain'),
+            webPath: this.node.tryGetContext('webPath'),
+            apiPath: this.node.tryGetContext('apiPath'),
+        });
+    }
+}
+
+const app = new cdk.App();
+
+new DynamicSiteStack(app, `DynamicSite-${app.node.tryGetContext('subdomain')}`, {
+    /**
+     * This is required for our use of hosted-zone lookup.
+     *
+     * Lookups do not work at all without an explicit environment
+     * specified; to use them, you must specify env.
+     * @see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+     */
+    env: {
+        account: app.node.tryGetContext('accountId'),
+        /**
+         * Stack must be in us-east-1, because the ACM certificate for a
+         * global CloudFront distribution must be requested in us-east-1.
+         */
+        region: 'us-east-1',
+    }
+});
+
+app.synth();
