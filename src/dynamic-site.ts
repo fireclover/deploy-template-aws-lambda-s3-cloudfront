2184619f905113c41e1949057b26cbec9f92afd5
Kim Daniel Engebretsen <kim@fireclover.cloud>
2025-04-08 14:21:40 +0200

    better spa handling

--- src/dynamic-site.ts
+++ src/dynamic-site.ts
@@ -118,49 +118,49 @@ export class DynamicSite extends Construct {
     new CfnOutput(this, 'Certificate', { value: certificate.certificateArn });
 
     // CloudFront distribution
-    const CFfunction = folderRedirects 
-      ? 'function handler(event) { \
-              var request = event.request; \
-              if (request.uri !== "/" && (request.uri.endsWith("/") || request.uri.lastIndexOf(".") < request.uri.lastIndexOf("/"))) { \
-                request.uri = request.uri.endsWith("/") ? request.uri.concat("index.html") : request.uri.concat("/index.html"); \
-              } \
-              return request; \
-            }'
-        : props.isSpa // FOR SPAs - subdirectory level where index.html is located.
-          ? 'var level = 0; \
-          var regexExpr = /^\/.+(\.\w+$)/; \
-          function handler(event) { \
-              var request = event.request; \
-              var olduri = request.uri; \
-              if (isRoute(olduri)) { \
-                  var defaultPath = ""; \
-                  var parts = olduri \
-                      .replace(/^\//,"") \
-                      .replace(/\/$/,"") \
-                      .split("/"); \
-                  var nparts = parts.length; \
-                  var limit = (level <= nparts) ? level : nparts; \
-                  for (var i = 0; i < limit; i++) { \
-                      defaultPath += "/" + parts[i]; \
-                  } \
-                  var newuri = defaultPath + "/index.html"; \
-                  request.uri = newuri; \
-                  console.log("Request for [" + olduri + "], rewritten to [" + newuri + "]"); \
-              } \
-              return request; \
-          }; \
-          function isRoute(uri) { \
-              return !regexExpr.test(uri); \
-          }'
-          : 'return {error: "not spa and not folder redirects. unsure what to do"}';
+    // const CFfunction = folderRedirects 
+    //   ? 'function handler(event) { \
+    //           var request = event.request; \
+    //           if (request.uri !== "/" && (request.uri.endsWith("/") || request.uri.lastIndexOf(".") < request.uri.lastIndexOf("/"))) { \
+    //             request.uri = request.uri.endsWith("/") ? request.uri.concat("index.html") : request.uri.concat("/index.html"); \
+    //           } \
+    //           return request; \
+    //         }'
+    //     : props.isSpa // FOR SPAs - subdirectory level where index.html is located.
+    //       ? 'var level = 0; \
+    //       var regexExpr = /^\/.+(\.\w+$)/; \
+    //       function handler(event) { \
+    //           var request = event.request; \
+    //           var olduri = request.uri; \
+    //           if (isRoute(olduri)) { \
+    //               var defaultPath = ""; \
+    //               var parts = olduri \
+    //                   .replace(/^\//,"") \
+    //                   .replace(/\/$/,"") \
+    //                   .split("/"); \
+    //               var nparts = parts.length; \
+    //               var limit = (level <= nparts) ? level : nparts; \
+    //               for (var i = 0; i < limit; i++) { \
+    //                   defaultPath += "/" + parts[i]; \
+    //               } \
+    //               var newuri = defaultPath + "/index.html"; \
+    //               request.uri = newuri; \
+    //               console.log("Request for [" + olduri + "], rewritten to [" + newuri + "]"); \
+    //           } \
+    //           return request; \
+    //       }; \
+    //       function isRoute(uri) { \
+    //           return !regexExpr.test(uri); \
+    //       }'
+    //       : 'return {error: "not spa and not folder redirects. unsure what to do"}';
 
 
 
     const defaultBehaviorViewerRequest = folderRedirects || props.isSpa ?  {
       functionAssociations: [{
           eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
-          function: new cloudfront.Function(this, 'Function', {
-            code: cloudfront.FunctionCode.fromInline(CFfunction),
+          function: new cloudfront.Function(this, 'CF-Function', {
+            code: cloudfront.FunctionCode.fromFile({ filePath: folderRedirects ? 'cf-function-folders.js' : props.isSpa ? 'cf-function-spa.js' : 'cf-function-default.js' }),
             runtime: cloudfront.FunctionRuntime.JS_2_0,
             autoPublish: true
           }),
