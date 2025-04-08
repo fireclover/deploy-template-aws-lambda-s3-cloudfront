c710f88f7187ea6f48ee8ce1407f8e5cb6e3ada3
Kim Daniel Engebretsen <kim@fireclover.cloud>
2025-04-08 12:24:32 +0200

    better spa handling

--- src/dynamic-site.ts
+++ src/dynamic-site.ts
@@ -126,22 +126,20 @@ export class DynamicSite extends Construct {
               } \
               return request; \
             }'
-        : props.isSpa 
-          ? 'var level = 0; // FOR SPAs - subdirectory level where index.html is located. \
-          var regexExpr = /^\/.+(\.\w+$)/; // Regex expression than matches paths requestiong an object. i.e: /route1/my-picture.png \
+        : props.isSpa // FOR SPAs - subdirectory level where index.html is located.
+          ? 'var level = 0; \
+          var regexExpr = /^\/.+(\.\w+$)/; \
           function handler(event) { \
               var request = event.request; \
               var olduri = request.uri; \
-              if (isRoute(olduri)) { // if is a route request. i.e: /route1 \
+              if (isRoute(olduri)) { \
                   var defaultPath = ""; \
                   var parts = olduri \
-                      .replace(/^\//,"") // remove leading "/" \
-                      .replace(/\/$/,"") // remove triling "/" if any \
-                      .split("/"); // split uri into array of parts. i.e: ["route1", "my-picture.png"] \
+                      .replace(/^\//,"") \
+                      .replace(/\/$/,"") \
+                      .split("/"); \
                   var nparts = parts.length; \
-                  // determine the limit as either level or nparts, whichever is lower \
                   var limit = (level <= nparts) ? level : nparts; \
-                  // build the default path. i.e: /route1 \
                   for (var i = 0; i < limit; i++) { \
                       defaultPath += "/" + parts[i]; \
                   } \
@@ -151,8 +149,6 @@ export class DynamicSite extends Construct {
               } \
               return request; \
           }; \
-          // Returns true if uri is a route. i.e: /route1 \
-          // Returns false if uri is a file. i.e: /route1/index.html \
           function isRoute(uri) { \
               return !regexExpr.test(uri); \
           }'
@@ -193,13 +189,13 @@ export class DynamicSite extends Construct {
       domainNames: [siteDomain],
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
       // logBucket: cloudFrontLoggingBucket.logBucket,
-      errorResponses:[
-        {
-          httpStatus: 403,
-          responseHttpStatus: 403,
-          responsePagePath: '/error.html',
-        }
-      ],
+      // errorResponses:[
+      //   {
+      //     httpStatus: 403,
+      //     responseHttpStatus: 403,
+      //     responsePagePath: '/error.html',
+      //   }
+      // ],
       defaultBehavior,
       additionalBehaviors: {
         'api/*': lambdaOrigin,
